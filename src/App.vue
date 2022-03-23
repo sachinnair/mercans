f4ff8bb16958dbb5a70b56ba2d61e15be55359f1
sachin <sachinkn6@gmail.com>
2022-03-23 08:04:05 +0530

    basic implementation

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
+
+import MenuLinks from '@/assets/menuLinks.json'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <nav>
+    <router-link 
+      v-for="menulink in MenuLinks.filter(x => x.path !== undefined)" 
+      :key="menulink.title"
+      :to="{ name: 'home', params: { menuitem: menulink.path}}">
+      {{menulink.title}}
+    </router-link>
+  </nav>
   <RouterView />
 </template>
 
@@ -25,20 +22,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 #app {
   max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
   font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 a,
@@ -49,8 +33,12 @@ a,
 }
 
 @media (hover: hover) {
-  a:hover {
+  /* a:hover {
     background-color: hsla(160, 100%, 37%, 0.2);
+  } */
+
+  nav a:hover {
+    background-color: rgba(var(--orange-rgb), 0.2);
   }
 }
 
@@ -59,10 +47,14 @@ nav {
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+  color: var(--menu-highlight-text);
+  background: var(--menu-highlight-background);
+  border-left: 4px solid var(--orange);
 }
 
 nav a.router-link-exact-active:hover {
@@ -73,46 +65,22 @@ nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+  color: var(--menu-item)
 }
 
 @media (min-width: 1024px) {
   body {
-    display: flex;
-    place-items: center;
+    margin: 0px;
   }
 
   #app {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   nav {
     text-align: left;
-    margin-left: -1rem;
     font-size: 1rem;
-
-    padding: 1rem 0;
     margin-top: 1rem;
   }
 }
