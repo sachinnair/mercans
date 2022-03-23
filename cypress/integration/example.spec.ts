429d36da658def9bb0e3a6e6a774d5bd5bafdbf7
sachin <sachinkn6@gmail.com>
2022-03-23 18:13:04 +0530

    e2e tests

--- cypress/integration/example.spec.ts
+++ cypress/integration/example.spec.ts
@@ -1,8 +1,10 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Test page on url hit', () => {
   it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.visit('/docs')
+    cy.contains('main', 'Checking out: docs')
+    cy.contains('leaves').click();
+    cy.url().should('eq', `${Cypress.config().baseUrl}/leaves`);
   })
 })
